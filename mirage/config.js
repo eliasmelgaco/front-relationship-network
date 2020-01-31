b83b9f91c071a02f7ac3fac0eecc95fc89f335fb
eliasmelgaco <eliasmelgaco@gmail.com>
2020-01-31 04:26:52 -0300

    Fix ember-mirage/mock

--- mirage/config.js
+++ mirage/config.js
@@ -1,12 +1,1 @@
-import { Response } from 'miragejs';
-
-export default function() {
-  this.get('/words', ({ words }, { queryParams }) => {
-    const foundWord = words.findBy({ nome: queryParams.nome });
-    if (foundWord) {
-      return foundWord;
-    }
-
-    return new Response(404, { 'Content-Type': 'application/json' }, { errors: [ 'Palavra não encontrada' ] });
-  });
-}
+export default function() { }
